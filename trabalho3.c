ace8ad43ccbe63de6762c2dbbbb8c8d576b67ff0
Pedro Lucas <pedrolpandrade@gmail.com>
2020-10-24 13:09:45 -0300

    Criada função do P2

--- trabalho3.c
+++ trabalho3.c
@@ -8,10 +8,46 @@
   Versão do SO: Ubuntu?
   Versão do gcc: usei 9.3.0
 ---------------------------------------------------------------------------*/
+#include<errno.h>
+#include<sys/types.h>
+#include<sys/ipc.h>
+#include<sys/msg.h>
+#include<stdlib.h>
+#include<stdio.h>
+#include<unistd.h>
 
 
+typedef struct mensagem{
+    long pid;
+    char msg[50];
+} t_mensagem;
 
 void P2(){
+    t_mensagem msg_env;
+    int i;
+    char msg_temp[50];
+    int pausa;
+
+    msg_env.pid = getpid();
+
+    for(i=0; i<10; i++){
+        //espera de 1 a 4 segundos
+        pausa = rand() % 4;
+        if(pausa == 0){
+            pausa++;
+        }
+        sleep(pausa);
+
+        //formata mensagem
+        sprintf(msg_temp, "pid=%d numero=%d", msg_env.pid, i);
+        strcpy(msg_env.msg, msg_temp);
+
+        //envia mensagem
+        msgsnd(idfila, &msg_env; sizeof(msg_env), 0);
+
+        printf("mensagem %s enviada\n", msg_env.msg);
+    }
+
 
 }
 
@@ -22,7 +58,17 @@ void P1(){
 
 
 int main(){
-    int pid;
+    int pid, idfila;
+    time_t t;
+
+    srand((unsigned) time(&t));
+
+
+    //criacao da fila
+    if((idfila = msgget(0x38316, IPC_CREAT|0x1ff)) < 0){
+        printf("Erro ao criar a fila de mensagens\n");
+        exit(1);
+    }
 
     //criacao do processo P2 (filho)
     if((pid = fork()) < 0){
